2e81e41e96cfbe2d3c3c451cf896a81b42ad2d48
Amarender <dodleamarender.reddy@gmail.com>
2023-06-04 17:53:20 +1000

    ..

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = file(var.credentials_file)
+  credentials = "gcloud-vm-388121-f17c2ed996e8.json"
   project = var.project
   region  = var.region
   zone    = var.zone
